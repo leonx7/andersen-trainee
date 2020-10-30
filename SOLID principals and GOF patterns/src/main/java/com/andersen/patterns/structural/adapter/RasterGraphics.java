8b79129281ae8871c3938407a51420c3411236e9
Leonid Ryzhyk <leonx7@tut.by>
2020-10-30 18:08:30 +0300

    feat: add Adapter design pattern

---
+++ SOLID principals and GOF patterns/src/main/java/com/andersen/patterns/structural/adapter/RasterGraphics.java
@@ -0,0 +1,11 @@
+package com.andersen.patterns.structural.adapter;
+
+public class RasterGraphics {
+    public void drawRasterLine(){
+        System.out.println("Drawing line");
+    }
+
+    public void drawRasterSquare(){
+        System.out.println("Drawing square");
+    }
+}
