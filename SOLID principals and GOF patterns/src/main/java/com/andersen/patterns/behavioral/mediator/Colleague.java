a6da2eb89493727e5343d64cda925e573c337d15
Leonid Ryzhyk <leonx7@tut.by>
2020-10-30 11:27:52 +0300

    feat: add Mediator design pattern

---
+++ SOLID principals and GOF patterns/src/main/java/com/andersen/patterns/behavioral/mediator/Colleague.java
@@ -0,0 +1,15 @@
+package com.andersen.patterns.behavioral.mediator;
+
+public abstract class Colleague {
+    private Mediator mediator;
+
+    public Colleague(Mediator mediator) {
+        this.mediator = mediator;
+    }
+
+    public void send(String message) {
+        mediator.send(message, this);
+    }
+
+    public abstract void notify(String message);
+}
