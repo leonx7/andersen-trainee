dfbb5f071e6275769d4af507c68cd5743ad43d08
Leonid Ryzhyk <leonx7@tut.by>
2020-11-18 12:21:58 +0300

    feat: add an example of using the synchronized block

---
+++ multithreading/src/main/java/Test3.java
@@ -0,0 +1,24 @@
+public class Test3 {
+    private static int counter = 0;
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread thread1 = new Thread(Test3::increment);
+        Thread thread2 = new Thread(Test3::increment);
+
+        thread1.start();
+        thread2.start();
+
+        thread1.join();
+        thread2.join();
+
+        System.out.println(Thread.currentThread().getName() + ": " + counter);
+    }
+
+    public static void increment() {
+        synchronized (Test3.class) {
+            for (int i = 0; i < 10000; i++) {
+                counter++;
+            }
+        }
+    }
+}
